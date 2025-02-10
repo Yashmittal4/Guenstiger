72b3643f87a233a5a534a0bade30231feac0451a
yash <yashmittal4949@gmail.com>
2025-02-11 01:29:14 +0530

    fix error of deployment

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from '../basic/Navbar'
-import Footer from '../basic/Footer'
+import Navbar from '../Basic/Navbar'
+import Footer from '../Basic/Footer'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
